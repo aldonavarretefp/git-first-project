512d36a5c6547629b92f2f19ae46411b33686c9d
navarretealdofp <aaldiitoo@gmail.com>
2021-05-18 19:39:22 -0500

    Creando los primeros botones de animales

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo")
+const $buttons = document.querySelectorAll("a")
+$buttons.forEach(btn =>{
+    //Adding an eventListener
+    btn.addEventListener("click",(e)=>{
+        let x = e.clientX - e.target.offsetLeft;
+        let y =e.clientY - e.target.offsetTop;
+        
+        let ripples = document.createElement('span');
+        ripples.style.left = x+'px';
+        ripples.style.top = y+'px';
+        btn.appendChild(ripples);
+
+        setTimeout(()=>{
+            ripples.remove()
+        },900)
+        console.log("Apretaste una mascota")
+    })
+}
+)
+
+$buttons.forEach(btn=>{
+    console.log(btn)
+})
